ee8144556d133756a330bb01e6de9014d7b1c749
leonardomartelli <leonardo@martellioliveira.me>
2023-08-31 22:23:38 -0300

    styling common button

--- src/components/buttons/common-button/common-button.component.tsx
+++ src/components/buttons/common-button/common-button.component.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { CommonButtonContract } from "./common-button.interface";
+import styles from "./common-button.module.scss";
 
 export function CommonButton(props: CommonButtonContract) {
   return (
-    <button onClick={props.onClick}>
+    <button onClick={props.onClick} className={styles.button}>
       <img src={props.icon} />
       {props.label}
     </button>
