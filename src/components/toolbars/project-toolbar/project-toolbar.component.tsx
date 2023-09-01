bd2599d1e5397976bc250438cb87c56d31efe1cd
leonardomartelli <leonardo@martellioliveira.me>
2023-08-31 22:25:12 -0300

    styling toolbar and using a label as button

--- src/components/toolbars/project-toolbar/project-toolbar.component.tsx
+++ src/components/toolbars/project-toolbar/project-toolbar.component.tsx
@@ -1,16 +1,10 @@
 import { CommonButton } from "../../buttons/common-button/common-button.component";
 import React, { useRef } from "react";
+import styles from "./project-toolbar.module.scss";
 import { ProjectToolBarContract } from "./project-toolbar.interface";
-import { FileSelection } from "../../file-selection/file-selection.component";
 
 export function ProjectToolBar(props: ProjectToolBarContract) {
   const exportProject = () => {};
-
-  const importProject = () => {
-    // fileSelectionRef?.current?.click();
-  };
-
-  const fileSelectionRef = useRef<HTMLInputElement>(null);
 
   const onFileLoaded = async (file: File) => {
     const project = JSON.parse(await file.text());
@@ -18,27 +12,29 @@ export function ProjectToolBar(props: ProjectToolBarContract) {
     props.updateProject(project);
   };
 
-  let style = {
-    background: "#FF0000",
-    width: "100%",
+  const onFileSelected = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target && event.target.files) {
+      let file = event.target.files[0];
+
+      if (file) {
+        onFileLoaded(file);
+      }
+    }
   };
 
   return (
-    <div style={style}>
-      <FileSelection
-        inputReference={fileSelectionRef}
-        onFileSelection={onFileLoaded}
-      />
-      <CommonButton
-        onClick={exportProject}
-        label="Salvar Projeto"
-        icon="arrow.down.doc.svg"
-      />
-      <CommonButton
-        onClick={importProject}
-        label="Carregar Projeto"
-        icon="arrow.up.doc.svg"
-      />
+    <div className={styles.toolbar}>
+      <label>
+        <input
+          type="file"
+          hidden={true}
+          onChange={(e) => {
+            onFileSelected(e);
+          }}
+        />
+        <img src="images/arrow.up.doc.svg" />
+        Upload
+      </label>
     </div>
   );
 }
