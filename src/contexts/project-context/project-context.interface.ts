185f0178e6bd185362d3990eba5419f357278dd7
leonardomartelli <leonardo@martellioliveira.me>
2023-09-03 23:05:55 -0300

    deleting useles context

--- src/contexts/project-context/project-context.interface.ts
+++
@@ -1,7 +0,0 @@
-import { Project } from "../../models/project/project.model";
-
-export interface ProjectContextContract {
-  ProjectObject?: Project;
-  exportProject: () => string;
-  importProject: (project: string) => void;
-}
