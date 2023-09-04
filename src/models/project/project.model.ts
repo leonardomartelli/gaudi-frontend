995a7c2cba839652d8e89adc95ee20d5d3665b3c
leonardomartelli <leonardo@martellioliveira.me>
2023-09-03 23:06:14 -0300

    using default project on Project class

--- src/models/project/project.model.ts
+++ src/models/project/project.model.ts
@@ -19,3 +19,48 @@ export class Project {
     this.filterIndex = filterIndex;
   }
 }
+
+export const defaultProject: Project = {
+  domain: {
+    materialProperties: {
+      elasticity: 0.3,
+      density: 1,
+      flow: 0.3,
+    },
+    dimensions: {
+      width: 120,
+      height: 60,
+    },
+    volumeFraction: 0.4,
+  },
+  boundaryConditions: {
+    supports: [
+      {
+        position: {
+          x: 0,
+          y: 0,
+        },
+        type: 1,
+      },
+      {
+        position: {
+          x: 0,
+          y: 60,
+        },
+        type: 1,
+      },
+    ],
+    forces: [
+      {
+        load: -1,
+        orientation: 1,
+        position: {
+          x: 120,
+          y: 30,
+        },
+      },
+    ],
+  },
+  penalization: 3,
+  filterIndex: 5.4,
+};
