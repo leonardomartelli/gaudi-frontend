cd49ade0e70fb9479df0c0d76aa737fc9f3848f1
leonardomartelli <leonardo@martellioliveira.me>
2023-09-03 23:04:10 -0300

    adding colors as code constants

---
+++ src/assets/constants.ts
@@ -0,0 +1,15 @@
+const HONOLULU_BLUE = "#asasas";
+const POPPY = "#da3540";
+const davys_gray = "#asasas";
+const ALICE_BLUE = "#e8f1f2";
+const frog_green = "#asasas";
+
+const out = {
+  HONOLULU_BLUE,
+  POPPY,
+  davys_gray,
+  ALICE_BLUE,
+  frog_green,
+};
+
+export default out;
