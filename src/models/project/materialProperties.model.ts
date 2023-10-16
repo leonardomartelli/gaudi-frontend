4fc30366532a670973b4df78462adc38b8ad42ed
leonardomartelli <leonardo@martellioliveira.me>
2023-10-15 22:05:59 -0300

    removing useles prop from material properties

--- src/models/project/materialProperties.model.ts
+++ src/models/project/materialProperties.model.ts
@@ -1,11 +1,9 @@
 export class MaterialProperties {
   elasticity: number;
   density: number;
-  flow: number;
 
-  constructor(elasticity: number, density: number, flow: number) {
+  constructor(elasticity: number, density: number) {
     this.elasticity = elasticity;
     this.density = density;
-    this.flow = flow;
   }
 }
